e2c6d253db962a071992e009e084968f293f881c
kesavindia <kesavindia83@gmail.com>
2023-06-27 15:03:23 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,35 @@ import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
+  const [id,setId] = React.useState()
+  const [loading,setLoading] = React.useState(false)
+  const [userData, setUserData] = React.useState()
+  const handleChange = ()=>{
+    setId(e.target.value)
+    getData(e.target.value)
+  }
+  async function getData(id){
+    try{
+        setLoading(true)
+        const res = await fetch('https://jsonplaceholder.typicode.com/photos/${id}')
+        const data = await res.json()
+        setUserData(data)
+    }
+    catch(err){
+
+    }
+    finally{
+        setLoading(false);
+    }
+  }
+  return(
+    <div>
+     <input value={id} onChange={handleChange} type='number'/>
+     {
+        loading?<Loader/>: !loading && userData && id !=0? <PhotoFrame url= {userData.url} title={userData.title}/>:null
+     }
+     </div>
+  )
 }
 
 
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = ({url,title}) => {
+   return(
+    <>
+        <div className="photoframe">
+            <img src= {url}/> 
+            <div className='caption'>{title}</div>   
+        </div>
+    </>
+   )
 }
